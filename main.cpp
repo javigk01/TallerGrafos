54cadba350a51c5fb698a5d6b857e4ca12d3b8a9
Javier Felipe Aldana Jaramillo <al_javier@javeriana.edu.co>
2025-05-17 01:31:26 +0000

    FINAAAAAAAL FINAAAAAAAAAL

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
+#include <iostream>
 #include "Perforadora.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Error: Debe especificar el nombre del archivo de entrada.\n";
+        std::cerr << "Uso: " << argv[0] << " archivo_entrada.txt\n";
+        return 1;
+    }
+
+    std::string archivoEntrada = argv[1];
     Perforadora p;
-    p.procesarCircuitosDesdeArchivo("entrada.txt");
+    p.procesarCircuitosDesdeArchivo(archivoEntrada);
+
     return 0;
 }
