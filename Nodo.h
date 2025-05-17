ea4fd1ea58dd607acc4039f000dc878aaf3571ec
JuanesDT027 <juanesdiaztoledo@gmail.com>
2025-05-17 00:12:20 +0000

    Implementación completa del taller de grafos

--- Nodo.h
+++ Nodo.h
@@ -3,8 +3,10 @@
 
 struct Nodo {
     float x, y;
-
-    Nodo(float x = 0.0, float y = 0.0) : x(x), y(y) {}
+    Nodo(float x = 0.0, float y = 0.0); // Declaración
 };
+
+// Incluir implementación directamente aquí para asegurar la vinculación
+#include "Nodo.hxx"
 
 #endif
---
+++ Nodo.hxx
@@ -0,0 +1,8 @@
+#ifndef NODO_HXX
+#define NODO_HXX
+
+#include "Nodo.h"
+
+inline Nodo::Nodo(float x, float y) : x(x), y(y) {}
+
+#endif
